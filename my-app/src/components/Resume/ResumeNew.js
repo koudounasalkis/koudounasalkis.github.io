d7d6058efff657e3a7b698cc64d01304df23fd92
koudounasalkis <alkis.koudounas@polito.it>
2022-10-27 20:19:05 +0000

    added files

--- my-app/src/components/Resume/ResumeNew.js
+++ my-app/src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_new.pdf";
+import pdf from "../../Assets/CV_new.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
