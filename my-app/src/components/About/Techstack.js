a50a20fb0cc53babb8fd5f837ed8d8ffcedfd098
koudounasalkis <alkis.koudounas@polito.it>
2022-10-27 19:46:03 +0000

    added files

---
+++ my-app/src/components/About/Techstack.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiJava,
+  DiCss3,
+  DiPython,
+  DiHtml5,
+} from "react-icons/di";
+import {
+  SiPytorch,
+  SiTensorflow,
+  SiApachespark,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPytorch />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTensorflow />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiApachespark />
+      </Col>
+
+
+
+
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ my-app/src/components/Home/Home2.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              Let me <span className="purple"> Introduce </span> Myself
+            </h1>
+            <p className="home-about-body">
+
+              I am a 
+              <i>
+                <b className="purple"> half Greek-half Italian Ph.D. student, </b>
+              </i>
+              deeply astonished by the mystic 
+              <i>
+                <b className="purple"> AI kingdom </b>
+              </i>
+              we are living. 
+              My current research topics are in the field of 
+              <i>
+                <b className="purple"> NLP and Audio Processing. </b>
+              </i>
+              
+              <br />
+              <br /> 
+              I treasure listening to 
+              <i>
+                <b className="purple"> music </b>
+              </i>
+              , 
+              <i>
+                <b className="purple"> travelling </b>
+              </i>
+              and taking
+              <i>
+                <b className="purple"> night-ish photos. </b>
+              </i>
+
+              <br />
+              <br />
+              I do relish 
+              <i>
+                <b className="purple"> space </b> 
+              </i>
+              and the immensity that surrounds it.
+              
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            {/* <h3>FIND ME ON</h3> */}
+            <p>
+              Feel free to <span className="purple">connect </span>with me!
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/koudounasalkis"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/AlkisKoudounas"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/alkis-koudounas/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/alkis_koudounas/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
