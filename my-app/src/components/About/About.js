8d3d06eccc8211fc7859e4b028227a354c08320a
koudounasalkis <alkis.koudounas@polito.it>
2022-10-27 20:34:27 +0000

    added files

--- my-app/src/components/About/About.js
+++ my-app/src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
+// import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
