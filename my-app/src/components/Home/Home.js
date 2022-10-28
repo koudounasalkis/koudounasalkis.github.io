90399c029c87baf7ad809d9862442b2f2b131976
koudounasalkis <alkis.koudounas@polito.it>
2022-10-28 13:51:40 +0000

    added files

--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
+// import homeLogo from "../../Assets/home-main.jpg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
