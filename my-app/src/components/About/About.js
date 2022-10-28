687b12dc7f4f09128937bf4cec7f569f329e2dd3
koudounasalkis <alkis.koudounas@polito.it>
2022-10-28 15:04:04 +0000

    added files

--- my-app/src/components/About/About.js
+++ my-app/src/components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 // import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/profile1.png";
+import laptopImg from "../../Assets/about1.png";
 import Toolstack from "./Toolstack";
 
 function About() {
