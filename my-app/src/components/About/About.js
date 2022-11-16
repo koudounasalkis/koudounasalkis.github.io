abf102b7b9e2875d67fda734428b20df4d362bee
koudounasalkis <alkis.koudounas@polito.it>
2022-11-16 12:21:41 +0000

    added public info

--- my-app/src/components/About/About.js
+++ my-app/src/components/About/About.js
@@ -7,51 +7,62 @@ import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about1.png";
 import Toolstack from "./Toolstack";
 
+import { Helmet } from 'react-helmet';
+
 function About() {
   return (
-    <Container fluid className="about-section">
-      <Particle />
-
-      <Container>
+      <Container fluid className="about-section">
         
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              About <strong className="purple">Me</strong>
-            </h1>
-            <Aboutcard />
-          </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-            <img src={laptopImg} alt="alkis koudounas about me" className="img-fluid" />
-          </Col>
-        </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
+        <Helmet>
+          <title>Alkis Koudounas | A little bit about me</title>
+          <meta name="description" content="This page describes a little bit who I am, what I do for living nd what I love to do as hobbies." />
+          <meta name="keywords" content="Alkis Koudounas,PhD,Politecnico,Torino,Amazon Alexa,About me,Technical skills,Portfolio" />
+          <meta name="author" content="Alkis Koudounas" />
+          <meta name="robots" content="index, follow" />
+        </Helmet>
 
-        <Techstack />
+        <Particle />
 
-        <h1 className="project-heading">
-          <strong className="purple">Adobe</strong> Suite
-        </h1>
-        <Toolstack />
+        <Container>
+          
+          <Row style={{ justifyContent: "center", padding: "10px" }}>
+            <Col
+              md={7}
+              style={{
+                justifyContent: "center",
+                paddingTop: "30px",
+                paddingBottom: "50px",
+              }}
+            >
+              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                About <strong className="purple">Me</strong>
+              </h1>
+              <Aboutcard />
+            </Col>
+            <Col
+              md={5}
+              style={{ paddingTop: "120px", paddingBottom: "50px" }}
+              className="about-img"
+            >
+              <img src={laptopImg} alt="alkis koudounas about me" className="img-fluid" />
+            </Col>
+          </Row>
+          <h1 className="project-heading">
+            Professional <strong className="purple">Skillset </strong>
+          </h1>
 
-        <Github />
+          <Techstack />
+
+          <h1 className="project-heading">
+            <strong className="purple">Adobe</strong> Suite
+          </h1>
+          <Toolstack />
+
+          <Github />
+
+        </Container>
 
       </Container>
-
-    </Container>
   );
 }
 
